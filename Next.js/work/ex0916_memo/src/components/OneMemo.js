dff6c4391ab5ef537b988e6cb534f13220bed416
park990 <rhkgkrwk2008@gmail.com>
2025-09-22 23:10:32 +0900

    memo

--- Next.js/work/ex0916_memo/src/components/OneMemo.js
+++ Next.js/work/ex0916_memo/src/components/OneMemo.js
@@ -8,7 +8,7 @@ return(
             <CardContent>
                         <div className="list-item">
                         <h4 className="item-list-h4">{memoData.idx}번 메모</h4>
-                        <h5><stronger>내용:</stronger>{memoData.content}:</h5>
+                        <h5><stronger>내용:</stronger>{memoData.content}</h5>
                         <p className="item-list-p">{memoData.writer}/{memoData.reg_date}</p>
                         </div>
             </CardContent>
